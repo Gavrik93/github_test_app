4db457619241a4f8590e24f0707ca27f36717739
Gaivoronskii Yegor <y.gaivoronskii@sharksw.com>
2020-01-07 15:20:10 +0200

    add parcing part to main model

--- lib/models/github_commit_model.dart
+++ lib/models/github_commit_model.dart
@@ -1,7 +1,14 @@
+import 'dart:convert';
+
 import 'commit.dart';
 import 'commit_author.dart';
 
 
+List<CommitModel> gitCommitModelFromJson(String str)=>
+   List<CommitModel>.from(jsonDecode(str).map((m)=>CommitModel.fromJson(m)));
+
+String gitCommitToJson(List<CommitModel> data) =>
+  jsonEncode(List<dynamic>.from(data.map((m)=> m.toJson())));
 
 
 class CommitModel {
@@ -10,5 +17,15 @@ class CommitModel {
   CommitAuthor author;
   CommitAuthor commiter;
 
-  
+  factory CommitModel.fromJson(Map<String, dynamic> json) => CommitModel(
+    commit: Commit.fromJson(json['commit']),
+    author: CommitAuthor.fromJson(json['author']),
+    commiter: CommitAuthor.fromJson(json['commiter'])
+  );
+
+  Map<String,dynamic> toJson() => <String,dynamic>{
+    'commit': commit.toJson(),
+    'author': author.toJson(),
+    'commiter': commiter.toJson(),
+  };
 }
